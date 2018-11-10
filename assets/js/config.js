66c7718fc1b92f4b6b505b4182251f85f892b415
Jim Ritchey <jim0ritchey@gmail.com>
2018-11-10 12:42:49 -0500

    Pushed html from master

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAvhHsfODB_Ig0MUCq6dn71JURli3SLJzI",
+    authDomain: "triviagame-e40d7.firebaseapp.com",
+    databaseURL: "https://triviagame-e40d7.firebaseio.com",
+    projectId: "triviagame-e40d7",
+    storageBucket: "triviagame-e40d7.appspot.com",
+    messagingSenderId: "34711059818"
+};
+firebase.initializeApp(config);
